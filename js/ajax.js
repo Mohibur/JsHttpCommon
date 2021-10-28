64c68a10a7117d110f32b09660aef1ea47443b59
johny <johny@cms.intra.rakuten.co.jp>
2021-10-28 15:21:52 +0900

    Seemingly Complete

--- js/ajax.js
+++ js/ajax.js
@@ -5,12 +5,17 @@ Ajax = function () {
 		return new Ajax(url);
 	}
 
-	var xmlhttp = null;
+	const ERROR_METHOD = "error";
+	const SUCCESS_METHOD = "success";
+	const AFTER_METHOD = "after";
+
+	var xmlhttp = new XMLHttpRequest();
 	var allData = {};
-	var url = null;
+	var url = "/";
+	var asynchronous  = true;
 	// all response would be registerd
 	var responseAction = {};
-	var subMeth = Methods.GET;
+	var subMethod = Methods.GET;
 
 	this.path = function(u) {
 		url = u;
@@ -26,9 +31,9 @@ Ajax = function () {
 		xmlhttp.setRequestHeader(name, value);
 		return this;
 	}
-	
+
 	this.on = function(responseCode, fnc) {
-		reponseCode = reponseCode.toString();
+		responseCode = responseCode.toString();
 		if(! /^[0-9]{3}$/.test(responseCode) ) {
 			throw "responseCode is invalid"
 		}
@@ -36,9 +41,18 @@ Ajax = function () {
 			throw "Function is expected as second parameter";
 		}
 
-		responseAction[reponseCode] = fnc;
+		responseAction[responseCode] = fnc;
+		return this;
 	}
 	
+	this.isAsync = function(isAasync) {
+		isAasync = isAasync.toString();
+		if(isAasync.toString().toLocaleLowerCase() != "true" && isAasync.toString().toLocaleLowerCase() != "false") {
+			throw "Either true or false is expected";
+		}
+		asynchronous = isAasync == "true";
+		return this;
+	}
 	this.data = function(d) {
 		allData = d;
 		return this;
@@ -57,16 +71,63 @@ Ajax = function () {
 		return this;
 	}
 
-	function processAfterSumit() {
-		
-	}
-	
-	
-	function bodySubmit(subString) {
-		
+	function buildString() {
+		let ret = [];
+		Object.keys(allData).forEach(function(key) {ret.push(encodeURIComponent(key) + "=" + encodeURIComponent(allData[key]))});
+		return ret.join("&");
 	}
 
-	
+	function process() {
+		// Create a map of header names to values
+		var headerMap = {};
+		var headerDump = xmlhttp.getAllResponseHeaders();
+		headerDump.trim().split(/[\r\n]+/).forEach(function (line) {
+			var parts = line.split(': ');
+			var header = parts.shift();
+			var value = parts.join(': ');
+			headerMap[header] = value;
+		});
+		
+		var contentType = headerMap["content-type"];
+		var body = bodyOriginal = xmlhttp.responseText;
+		var postProcessMethod = SUCCESS_METHOD;
+		
+		// setting mehtod based on success
+		if(xmlhttp.status < 200 || xmlhttp.status >= 300) {
+			postProcessMethod = ERROR_METHOD;
+		}
+
+		// parsing json response
+		var parsingError = false;
+		if(contentType.toLocaleLowerCase().indexOf("json") > -1) {
+			try {
+				body = JSON.parse(body);
+			} catch (err) {
+				body = err;
+				postProcessMethod = ERROR_METHOD;
+				parsingError = true;
+			}
+		}
+
+		// error code is high priority
+		if(typeof responseAction[xmlhttp.status.toString()] == "function" && !parsingError) {
+			responseAction[xmlhttp.status.toString()](body, headerMap, {status: xmlhttp.status, rawBody: bodyOriginal, rawHeader: headerDump});
+		}
+		// open, error is low priority
+		if(typeof responseAction[postProcessMethod] == "function") {
+			responseAction[postProcessMethod](body, headerMap, {status: xmlhttp.status, rawBody: bodyOriginal, rawHeader: headerDump});
+		}
+		// finally
+		if(typeof responseAction[AFTER_METHOD] == "function") {
+			responseAction[AFTER_METHOD]();
+		}
+	}
+
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState === 4) {
+			process();
+		}
+	}
 
 	this.json = function () {
 		if(subMethod == Methods.GET) {
@@ -74,13 +135,49 @@ Ajax = function () {
 		}
 		var subString = typeof allData == "string" ? allData : JSON.stringify(allData);
 		this.header("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
-		xmlhttp.open(method, url);
+		xmlhttp.open(subMethod, url, asynchronous);
 		xmlhttp.send(subString);
-		processAfterSumit()
 	}
 
 	this.post = function() {
-		
+		subMethod = Methods.POST;
+		xmlhttp.open(subMethod, url, asynchronous);
+		xmlhttp.send(buildString());
+	}
+
+	this.get = function() {
+		subMethod = Methods.GET;
+		let spls = url.split("?");
+		let uri = spls.length > 1 ? spls[1] + "&" : "";
+		xmlhttp.open(subMethod, spls[0] + "?" + uri + buildString(), asynchronous);
+		xmlhttp.send();
+	}
+
+	this.success = function(fnc) {
+		if(typeof fnc != "function") {
+			throw "have to be a funciton";
+		}
+
+		responseAction[SUCCESS_METHOD] = fnc;
+		return this;
+	}
+
+	this.error = function(fnc) {
+		if(typeof fnc != "function") {
+			throw "have to be a funciton";
+		}
+
+		responseAction[ERROR_METHOD] = fnc;
+		return this;
+	}
+	
+	
+	this.after = function(fnc) {
+		if(typeof fnc != "function") {
+			throw "have to be a funciton";
+		}
+		responseAction[AFTER_METHOD] = fnc;
+		return this;
 	}
 
 }
