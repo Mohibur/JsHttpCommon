f669506e98def6abb66d42216e4abe42722dc11b
johny <johny@jshttpcommon.com>
2021-11-03 08:51:34 +0900

    Improvement

--- js/submitform.js
+++ js/submitform.js
@@ -3,11 +3,7 @@ const FORM_SUBMIT_VER = "1.0.0";
 
 function __form_submit() {
 	return new function() {
-		let form = document.createElement("form");
-		form.style.display = "none";
-		form.id = window.URL.createObjectURL(new Blob([])).substr(-36);
-		document.body.appendChild(form);
-
+		let form = null;
 		this.value = function(name, val) {
 			let input = document.createElement("input");
 			input.type = "hidden";
@@ -28,20 +24,40 @@ function __form_submit() {
 			form.method = method;
 			return this;
 		}
+		
+		/*
+		 * 
+		 *
+		 * */
+		this.reset = function() {
+			form.innerHTML = "";
+			return this;
+		}
+
+		let create = function() {
+			form = document.createElement("form");
+			form.style.display = "none";
+			form.id = window.URL.createObjectURL(new Blob([])).substr(-36);
+
+		}
 
 		this.submit = function() {
+			document.body.appendChild(form);
 			form.submit();
 		}
 
 		this.post = function() {
 			form.method = Methods.POST;
+			document.body.appendChild(form);
 			form.submit();
 		}
 
 		this.get = function() {
 			form.method = Methods.GET;
+			document.body.appendChild(form);
 			form.submit();
 		}
+		create();
 	}
 }
 
--- test/submit.php
+++ test/submit.php
@@ -6,9 +6,18 @@ echo $_SERVER['REQUEST_METHOD'] . " method received\n";
 
 echo "Input found: \n";
 
-if($_SERVER['REQUEST_METHOD'] == "GET") {
-	print_r($_GET);
-} else {
-	$rawdata = file_get_contents("php://input");
-	echo $rawdata . "\n"; 
-}
+$age_range[0] = "0 - 4";
+$age_range[1] = "5 - 12";
+$age_range[2] = "13 - 19";
+$age_range[3] = "20 - 29";
+$age_range[4] = "30 - 39";
+$age_range[5] = "40 - 49";
+$age_range[6] = "50 - 59";
+$age_range[7] = "60 - 69";
+$age_range[8] = "70 - 79";
+$age_range[9] = "80 - above";
+
+echo "    > Name: " . $_REQUEST["name"] . "\n";
+echo "    > Age Range: " . $age_range[$_REQUEST["age_range"] - 1] . "\n";
+
+
